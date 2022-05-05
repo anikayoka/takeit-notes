b82995ebac16c71587e6b3608646dbba783f7ff7
sunchaser <anika.mclean@gmail.com>
2022-05-05 13:55:16 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-
-
 const PORT = process.env/PORT || 3001;
 const app = express();
 
@@ -8,3 +6,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+
+app.use(require('./route/routeapi'));
+app.use(require('./route/routehtml'));
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log('App listening on PORT.....,',PORT)
+})
