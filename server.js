dcc40e6eca88add3f3a91ef9539e540355efe611
sunchaser <anika.mclean@gmail.com>
2022-05-05 13:19:40 -0400

    add routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
+
 const notes = require('./db/db.json');
 
 const PORT = process.env/PORT || 3001;
@@ -9,3 +9,4 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
+
