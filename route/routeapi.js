31820c2a21df25c8d2112411ef5123824f5dabcc
sunchaser <anika.mclean@gmail.com>
2022-05-05 13:47:35 -0400

    add api router

---
+++ route/routeapi.js
@@ -0,0 +1,43 @@
+const router = require("express").Router()
+
+const uuid = require('uuid');
+
+const fs = require('fs');
+
+let notes = require('./db/db.json');
+
+router.get ("/api/notes", (req,res) => {
+  notes = JSON.parse(fs.readFileSync('/db/db.json'))
+  res.json(notes)
+})
+
+router.post ("/api/notes", (req,res) => {
+  const newNote = {
+    id:uuid.v4(),
+    title:req.body.title,
+    text:req.body.text
+  }
+  notes.push(newNote)
+
+  fs.writeFileSync('/db/db.json', JSON.stringify(notes),(err,data) => {
+    if(err) throw err;
+  })
+  res.json(notes)
+})
+
+router.delete ("/api/notes/:id", (req,res) => {
+ let adjustNote = []
+ notes.forEach(element => {
+   if (element.id != req.params.id){
+     adjustNote.push(element)
+   }
+ });
+  notes=adjustNote
+
+  fs.writeFileSync('/db/db.json', JSON.stringify(notes),(err,data) => {
+    if(err) throw err;
+  })
+  res.json(notes)
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const fs = require('fs');
 
-const notes = require('./db/db.json');
 
 const PORT = process.env/PORT || 3001;
 const app = express();
